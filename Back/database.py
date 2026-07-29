779f8d3268f97575ccb5cba1711b669a00781c6b
GabrHenr <gabrielhenriquedxd@gmail.com>
2026-07-29 11:40:00 -0300

    Update: Fábrica de sessoes

--- Back/database.py
+++ Back/database.py
@@ -1,9 +1,21 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = 'postgresql://postgres:1234@localhost:5432/PlayTest'
 engine = create_async_engine(url = DATABASE_URL, echo = True)
 
+
+session_factory = async_sessionmaker(
+    bind= engine,
+    class_= AsyncSession,
+    expire_on_commit= False
+)
+
+async def get_session():
+    async with session_factory() as session:
+        yield session
+    
+    
 class Base(DeclarativeBase):
     pass
