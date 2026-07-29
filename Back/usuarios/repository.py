a83913545735a9fcb752720ca090d968b5a0c11c
GabrHenr <gabrielhenriquedxd@gmail.com>
2026-07-29 11:35:13 -0300

    Updates: Adicionando usuarios com schema e repositorye fábrica de sessoes

---
+++ Back/usuarios/repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from models import Usuarios
+
+
+class UsuarioRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def recuperar_usuario_por_email(self, email: str):
+        statement = (
+            select(Usuarios).select_from(Usuarios).where(Usuarios.email == email)
+        )
+        resultado = await self.session.execute(statement)
+        usuario = resultado.scalar_one_or_none()
+        return usuario
---
+++ Back/usuarios/schema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from enums import AcessoTipos
+class LoginRequest(BaseModel):
+    email: str
+    senha: str
+
+class LoginResponse(BaseModel):
+    nome: str
+    sobrenome: str
+    acesso: AcessoTipos
